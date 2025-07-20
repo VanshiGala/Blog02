7b2745397e737caf918ca8018a44ef044932dab4
VanshiGala <galavanshi0207@gmail.com>
2025-07-20 19:19:46 +0530

    change

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { checkForAuthentiationCookie } from "../middlewares/authentication.js";
 const router = Router();
 
